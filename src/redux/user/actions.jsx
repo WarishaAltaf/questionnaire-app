08cc5146120541409d321a95625b0047e39a3145
MobileTSP <mobiletspdeveloper@gmail.com>
2024-11-10 23:19:23 +0500

    feat: redux store client side error resolved

--- src/redux/user/actions.jsx
+++ src/redux/user/actions.jsx
@@ -1,7 +1,20 @@
+"use client";
+import {
+  addInitialUserData,
+  getUserDataByEmail,
+} from "@/utils/supabase/actions";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
 export const getUserDetails = createAsyncThunk(
   "user/gettUserDetails",
-  async (_, { rejectWithValue }) => {
+  async (email, { rejectWithValue }) => {
     try {
+      let data = await getUserDataByEmail(email);
+      if (!data) {
+        data = await addInitialUserData(email);
+      }
+
+      return data;
     } catch (error) {
       return rejectWithValue(error || "An error occurred");
     }
--- src/redux/user/reducer.jsx
+++ src/redux/user/reducer.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getUserDetails } from "./actions";
 
